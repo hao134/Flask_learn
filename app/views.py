30701e2da577a3e4c420d89c982553edaaff26cf
hao134 <hsh319ab@gmail.com>
2023-01-04 17:57:07 +0800

    Working with jinja templates

--- app/views.py
+++ app/views.py
@@ -2,9 +2,68 @@ from app import app
 
 from flask import render_template
 
+from datetime import datetime
+
+@app.template_filter("clean_date")
+def clean_date(dt):
+    return dt.strftime("%d %b %Y")
+
 @app.route("/")
 def index():
     return render_template("public/index.html")
+
+@app.route("/jinja")
+def jinja():
+
+    my_name = "Shih hao"
+
+    age = 28
+
+    langs = ["Python", "Javascript", "Golang", "Bash"]
+
+    friends = {
+        "shinchi":27,
+        "an":27,
+        "lishi":48,
+        "liching":47
+    }
+
+    colours = ("Red", "Green")
+    cool = True
+
+    class GitRemote:
+        def __init__(self, name, description, url):
+            self.name = name
+            self.description = description
+            self.url = url
+
+        def pull(self):
+            return f"Pullin repo {self.name}"
+
+        def clone(self):
+            return f"Cloning into {self.url}"
+
+    my_remote = GitRemote(
+        name = "Flask Jinja",
+        description = "Template design tutorial",
+        url = "https://github.com/hao134/flask_learn"
+    )
+    def repeat(x, qty):
+        return x * qty
+
+    date = datetime.utcnow()
+
+    my_html = "<h1>THIS IS SOME HTML</h1>"
+
+    suspicious = "<script>alert('You got hacked!!!')</script>"
+
+    return render_template("public/jinja.html", my_name = my_name, age=age,
+                            langs=langs, friends=friends, colours=colours,
+                            cool=cool, GitRemote = GitRemote, repeat=repeat,
+                            my_remote = my_remote, date = date, my_html = my_html,
+                            suspicious=suspicious
+    )
+
 
 @app.route("/about")
 def about():
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 1. start: Serving HTML files : https://youtube.com/watch?v=QrK50lIwgbk&feature=shares
 2. Serving static files with Flask : https://youtube.com/watch?v=GxT0zgZSHE8&feature=shares
 3. Flask HTML templates with Jinja: https://youtube.com/watch?v=pj1iLRljwxI&feature=shares
+4. Working with Jinja templates : https://youtube.com/watch?v=mqrbF0qGSLI&feature=shares
